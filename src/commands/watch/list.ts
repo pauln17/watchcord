cf0d2ff7f4ef70cfcf3937a3f859eff6b47a82cb
Phuc <119803257+pauln17@users.noreply.github.com>
2026-04-20 03:54:19 -0400

    feat(cmds): basic watch list command setup

--- src/commands/watch/list.ts
+++ src/commands/watch/list.ts
@@ -1,5 +1,58 @@
-import { ChatInputCommandInteraction } from "discord.js";
+import {
+  ChannelType,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  MessageFlags,
+} from "discord.js";
 
-export const listWatch = async (interaction: ChatInputCommandInteraction) => {
-  return await interaction.reply(`list`);
+import type { IServices } from "../../services/initializeServices";
+
+export const listWatch = async (
+  interaction: ChatInputCommandInteraction,
+  services: IServices,
+) => {
+  if (!interaction.guild || !interaction.guildId) {
+    return await interaction.reply({
+      content: "This command can only be used in a guild",
+      flags: MessageFlags.Ephemeral,
+    });
+  }
+
+  const channel = interaction.options.getChannel("channel", false, [
+    ChannelType.GuildText,
+  ]);
+
+  const notificationEmbed = new EmbedBuilder()
+    .setColor("#5f58b6")
+    .setTitle(`Watch List of Server: ${interaction.guild.name}`)
+    .setFooter({
+      text: "Watchcord",
+      iconURL: interaction.client.user?.displayAvatarURL() ?? "",
+    })
+    .setTimestamp(new Date());
+
+  if (channel) {
+    const watches = await services.watchService.getUserWatchesByGuildAndChannel(
+      interaction.guildId,
+      channel.id,
+      interaction.user.id,
+    );
+
+    // Build Embed Description
+
+    notificationEmbed.setDescription(`TBD ${watches}`);
+  } else {
+    const watches = await services.watchService.getUserWatchesByGuild(
+      interaction.guildId,
+      interaction.user.id,
+    );
+
+    // Build Embed Description
+    notificationEmbed.setDescription(`TBD ${watches}`);
+  }
+
+  return await interaction.reply({
+    embeds: [notificationEmbed],
+    flags: MessageFlags.Ephemeral,
+  });
 };
--- src/commands/watch/watch.ts
+++ src/commands/watch/watch.ts
@@ -1,5 +1,10 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import {
+  ChatInputCommandInteraction,
+  InteractionContextType,
+  SlashCommandBuilder,
+} from "discord.js";
 
+import type { ExtendedClient } from "../../discord/ExtendedClient";
 import { addWatch } from "./add";
 import { editWatch } from "./edit";
 import { listWatch } from "./list";
@@ -9,10 +14,21 @@ import { viewWatch } from "./view";
 export const data = new SlashCommandBuilder()
   .setName("watch")
   .setDescription("Manage your watches")
+  .setContexts(InteractionContextType.Guild)
+
+  // List Subcommand
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("list")
+      .setDescription("List your watches")
+      .addChannelOption((option) =>
+        option
+          .setName("channel")
+          .setDescription("List watches by channel")
+          .setRequired(false),
+      ),
+  )
   .toJSON();
-
-// List Subcommand
-
 // View Subcommand
 
 // Add Subcommand
@@ -23,10 +39,11 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction: ChatInputCommandInteraction) {
   const subcommand = interaction.options.getSubcommand();
+  const services = (interaction.client as ExtendedClient).services;
   // TODO: Implement Interaction Responses
   switch (subcommand) {
     case "list":
-      return await listWatch(interaction);
+      return await listWatch(interaction, services);
     case "view":
       return await viewWatch(interaction);
     case "add":
