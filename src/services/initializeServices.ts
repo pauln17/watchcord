6dd95b055a05181f8084e1a6603601b1fe5dacd3
Phuc <119803257+pauln17@users.noreply.github.com>
2026-04-22 15:08:02 -0400

    feat: hydrate redis function on startup

--- src/services/initializeServices.ts
+++ src/services/initializeServices.ts
@@ -7,12 +7,36 @@ export interface IServices {
   watchConditionService: WatchConditionService;
 }
 
-export const initializeServices = (
+export const initializeServices = async (
   prisma: PrismaClient,
   redis: RedisClientType,
-): IServices => {
-  return {
-    watchService: new WatchService(prisma, redis),
-    watchConditionService: new WatchConditionService(prisma, redis),
+): Promise<IServices> => {
+  const watchService = new WatchService(prisma, redis);
+  const watchConditionService = new WatchConditionService(prisma, redis);
+
+  const services = {
+    watchService,
+    watchConditionService,
   };
+
+  await hydrateRedis(services, redis);
+
+  return services;
+};
+
+const hydrateRedis = async (services: IServices, redis: RedisClientType) => {
+  const watches = await services.watchService.getAllWatches();
+
+  for (const watch of watches) {
+    await redis.set(`watches:${watch.id}`, JSON.stringify(watch));
+
+    if (watch.scope === "GUILD") {
+      await redis.sAdd(`guilds:${watch.guildId}`, watch.id);
+    } else if (watch.scope === "CHANNEL" && watch.channelId) {
+      await redis.sAdd(
+        `guilds:${watch.guildId}:channels:${watch.channelId}`,
+        watch.id,
+      );
+    }
+  }
 };
