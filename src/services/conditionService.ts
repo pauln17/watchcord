562784a6f6e6376c72194c54fbb2a3dc05b3eb4d
Paul <paulnguyen0417@gmail.com>
2026-04-27 12:40:02 -0400

    feat: conditions service now updates watch cache

--- src/services/conditionService.ts
+++ src/services/conditionService.ts
@@ -1,6 +1,9 @@
+import type { RedisJSON } from "redis";
+
 import type { RedisClientType } from "../lib/redis";
 import type { IRepositories } from "../repositories";
 import type { Condition } from "../types";
+import type { RedisSearchResult, Watch } from "../types";
 import type { ILogger } from "../util/logger";
 
 export interface IConditionService {
@@ -32,9 +35,31 @@ export class ConditionService implements IConditionService {
   createUserCondition = async (
     data: Omit<Condition, "id">,
   ): Promise<Condition> => {
-    return await this.repositories.conditionRepository.create({
+    const condition = await this.repositories.conditionRepository.create({
       ...data,
     });
+
+    const result = (await this.redis.ft.search(
+      "idx:watches",
+      `@id:{"${condition.watchId}"}`,
+      {
+        LIMIT: {
+          from: 0,
+          size: 1,
+        },
+      },
+    )) as unknown as RedisSearchResult<Watch>;
+
+    if (result.total > 0 && result.documents[0]?.value != null) {
+      result.documents[0].value.conditions.push(condition);
+      await this.redis.json.set(
+        `wc:watches:${condition.watchId}`,
+        "$",
+        result.documents[0].value as unknown as RedisJSON,
+      );
+    }
+
+    return condition;
   };
 
   deleteUserCondition = async (
@@ -45,6 +70,32 @@ export class ConditionService implements IConditionService {
       await this.repositories.conditionRepository.findByIdAndUserId(id, userId);
     if (!existing) return null;
 
-    return await this.repositories.conditionRepository.deleteById(id);
+    const condition =
+      await this.repositories.conditionRepository.deleteById(id);
+
+    const result = (await this.redis.ft.search(
+      "idx:watches",
+      `@id:{"${existing.watchId}"}`,
+      {
+        LIMIT: {
+          from: 0,
+          size: 1,
+        },
+      },
+    )) as unknown as RedisSearchResult<Watch>;
+
+    if (result.total > 0 && result.documents[0]?.value != null) {
+      result.documents[0].value.conditions =
+        result.documents[0].value.conditions.filter(
+          (condition) => condition.id !== id,
+        );
+      await this.redis.json.set(
+        `wc:watches:${result.documents[0].value.id}`,
+        "$",
+        result.documents[0].value as unknown as RedisJSON,
+      );
+    }
+
+    return condition;
   };
 }
